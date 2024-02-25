e0f31cea7133c2da37a2a672c48fa7ae373bf36b
Arghya Chattapadhyay <arghyachatterjee984@gmail.com>
2024-02-25 15:59:39 +0530

    added comment feature for all post

--- scripts/post.js
+++ scripts/post.js
@@ -24,3 +24,20 @@ likebutton.addEventListener("click", ()=>{
     likecount.innerText="1 person likes this!";
     likebutton.className="fa-solid fa-thumbs-up";
 });
+
+
+//code for comments
+function submitComment() {
+  var commentInput = document.getElementById("commentInput").value;
+  var commentsList = document.getElementById("commentsList");
+  
+  // Create a new list item for the comment
+  var listItem = document.createElement("li");
+  listItem.textContent = commentInput;
+  
+  // Add the new comment to the top of the list
+  commentsList.insertBefore(listItem, commentsList.firstChild);
+  
+  // Clear the comment input field after submitting
+  document.getElementById("commentInput").value = "";
+}
