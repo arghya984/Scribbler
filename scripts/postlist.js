b6300dadc031a6ac4db00fee44c436b7af1ca418
Arghya Chattapadhyay <arghyachatterjee984@gmail.com>
2024-02-25 12:08:21 +0530

    edited the postlist page

---
+++ scripts/postlist.js
@@ -0,0 +1,5 @@
+const deletepost2=document.getElementById("trash2");
+const deleteModal=document.getElementsByClassName("modal")[0];
+deletepost2.addEventListener("click", ()=>{
+    deleteModal.style.display = "block";
+});
